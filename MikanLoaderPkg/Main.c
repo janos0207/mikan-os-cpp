98a6b7495ca8f911845947da0f9809f135cef1e0
yasu_fukuda <janos0207terminal@gmail.com>
2023-10-14 00:34:13 +0900

    add: GetMemoryMap()

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,3 +1,4 @@
+#include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 #include <Uefi.h>
 
@@ -7,4 +8,24 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
   while (1) {
   }
   return EFI_SUCCESS;
+}
+
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+                           &map->map_key, &map->descriptor_size,
+                           &map->descriptor_version);
 }
