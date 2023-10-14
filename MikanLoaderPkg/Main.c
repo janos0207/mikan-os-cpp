6007305d7eb31262604a97c32c5513a1aaf8ea79
yasu_fukuda <janos0207terminal@gmail.com>
2023-10-14 16:39:00 +0900

    add: SaveMemoryMap()

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,5 +1,10 @@
+#include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
+#include <Protocol/BlockIo.h>
+#include <Protocol/DiskIo2.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
 #include <Uefi.h>
 
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
@@ -10,16 +15,16 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
   return EFI_SUCCESS;
 }
 
-struct MemoryMap {
+typedef struct MemoryMap {
   UINTN buffer_size;
   VOID* buffer;
   UINTN map_size;
   UINTN map_key;
   UINTN descriptor_size;
   UINT32 descriptor_version;
-};
+} MemoryMap;
 
-EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+EFI_STATUS GetMemoryMap(MemoryMap* map) {
   if (map->buffer == NULL) {
     return EFI_BUFFER_TOO_SMALL;
   }
@@ -28,4 +33,71 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
   return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR*)map->buffer,
                            &map->map_key, &map->descriptor_size,
                            &map->descriptor_version);
+}
+
+const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+  switch (type) {
+    case EfiReservedMemoryType:
+      return L"EfiReservedMemoryType";
+    case EfiLoaderCode:
+      return L"EfiLoaderCode";
+    case EfiLoaderData:
+      return L"EfiLoaderData";
+    case EfiBootServicesCode:
+      return L"EfiBootServicesCode";
+    case EfiBootServicesData:
+      return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode:
+      return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData:
+      return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory:
+      return L"EfiConventionalMemory";
+    case EfiUnusableMemory:
+      return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory:
+      return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS:
+      return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO:
+      return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace:
+      return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode:
+      return L"EfiPalCode";
+    case EfiPersistentMemory:
+      return L"EfiPersistentMemory";
+    case EfiMaxMemoryType:
+      return L"EfiMaxMemoryType";
+    default:
+      return L"InvalidMemoryType";
+  }
+}
+
+EFI_STATUS SaveMemoryMap(MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+  CHAR8 buf[256];
+  UINTN len;
+
+  CHAR8* header =
+      "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attributes\n";
+  len = AsciiStrLen(header);
+  file->Write(file, &len, header);
+
+  Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer,
+        map->map_size);
+
+  EFI_PHYSICAL_ADDRESS iter;
+  int i;
+  for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
+       iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+       iter += map->descriptor_size, i++) {
+    EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+    len = AsciiSPrint(buf, sizeof(buf), "%u, %x, %-ls, %08lx, %lx, %ln\n", i,
+                      desc->Type, GetMemoryTypeUnicode(desc->Type),
+                      desc->PhysicalStart, desc->NumberOfPages,
+                      desc->Attribute & 0xffffflu);
+    file->Write(file, &len, buf);
+  }
+
+  return EFI_SUCCESS;
 }
